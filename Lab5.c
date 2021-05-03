99df92ca9359eeebcc5c1b12ca37f4e9ca2e4acc
DarkDevourer <kuhcrazan2000@mail.ru>
2021-05-03 18:22:35 +0300

    Ready Sobel Filter

--- Lab5.c
+++ Lab5.c
@@ -11,11 +11,11 @@
 #include <math.h>
 #include "bitmap.h"
 
-void to_bw(int data_size, unsigned char *imarr, unsigned char *resultarr, int pthread_cnt, int *right_border);
+void to_bw(int data_size, unsigned char *imarr, int pthread_cnt, int *right_border);
 
 void sobel_filter(unsigned char *imarr, unsigned char *resultarr, int pthread_cnt, int *right_border);
 
-void sobel(unsigned char *imarr, unsigned char *resultarr, int pthread_number, int left_border, int right_border);
+void sobel(unsigned char *imarr, unsigned char *resultarr, int pthread_number, int left_border, int right_border, int width, int height);
 
 int main(int argc, char *argv[])
 {
@@ -69,14 +69,15 @@ int main(int argc, char *argv[])
 	printf("%d\n",header->width);
 	printf("%d\n",header->height);
 	printf("%d\n",header->size);
-	to_bw(data_size, data, resultarr, pthread_cnt, NULL);
+	to_bw(data_size, data, pthread_cnt, NULL);
+	sobel(data, resultarr,pthread_cnt,0,0,header->width,header->height);
 
 	save_bitmap(argv[2], header, resultarr);
 
-	return 0;//
+	return 0;
 }
 
-void to_bw(int data_size, unsigned char *imarr, unsigned char *resultarr, int pthread_cnt, int *right_border)
+void to_bw(int data_size, unsigned char *imarr, int pthread_cnt, int *right_border)
 {
 	int pixel_cnt = data_size/3;
 	for (int i = 0; i < pixel_cnt; i++)
@@ -85,9 +86,9 @@ void to_bw(int data_size, unsigned char *imarr, unsigned char *resultarr, int pt
 		int b=imarr[i*3+1];
 		int c=imarr[i*3+2];
 		unsigned char av = (a+b+c)/3;
-		resultarr[i*3] = av;
-		resultarr[i*3+1] = av;
-		resultarr[i*3+2] = av;
+		imarr[i*3] = av;
+		imarr[i*3+1] = av;
+		imarr[i*3+2] = av;
 	}
 }
 
@@ -96,13 +97,29 @@ void sobel_filter(unsigned char *imarr, unsigned char *resultarr, int pthread_cn
 
 }
 
-void sobel(unsigned char *imarr, unsigned char *resultarr, int pthread_number, int left_border, int right_border)
+void sobel(unsigned char *imarr, unsigned char *resultarr, int pthread_number, int left_border,int right_border, int width, int height)
 {
-	float kernelx[3][3] = {{-1, 0, 1},
+	char sobel_x[3][3] = {{-1, 0, 1},
 						   {-2, 0, 2},
 						   {-1, 0, 1}}; 
-	float kernely[3][3] = {{-1, 2, -1},
+	char sobel_y[3][3] = {{-1, -2, -1},
 						   {0, 0, 0},
-						   {1, 2, 1}}; 		
+						   {1, 2, 1}}; 
+
+	for (int x = 1; x < width-1; x++)
+		for (int y = 1; y < height-1; y++)
+		{
+			unsigned int pixel_x = (sobel_x[0][0] * imarr[3*(x-1+(y-1)*width)]) + (sobel_x[0][1] * imarr[3*(x+(y-1)*width)]) + (sobel_x[0][2] * imarr[3*(x+1+(y-1)*width)]) +
+			(sobel_x[1][0] * imarr[3*(x-1+(y)*width)]) + (sobel_x[1][1] * imarr[3*(x+(y)*width)]) + (sobel_x[1][2] * imarr[3*(x+1+(y)*width)]) +
+			(sobel_x[2][0] * imarr[3*(x-1+(y+1)*width)]) + (sobel_x[2][1] * imarr[3*(x+(y+1)*width)]) + (sobel_x[2][2] * imarr[3*(x+1+(y+1)*width)]);
+
+			unsigned int pixel_y = (sobel_y[0][0] * imarr[3*(x-1+(y-1)*width)]) + (sobel_y[0][1] * imarr[3*(x+(y-1)*width)]) + (sobel_y[0][2] * imarr[3*(x+1+(y-1)*width)]) +
+			(sobel_y[1][0] * imarr[3*(x-1+(y)*width)]) + (sobel_y[1][1] * imarr[3*(x+(y)*width)]) + (sobel_y[1][2] * imarr[3*(x+1+(y)*width)]) +
+			(sobel_y[2][0] * imarr[3*(x-1+(y+1)*width)]) + (sobel_y[2][1] * imarr[3*(x+(y+1)*width)]) + (sobel_y[2][2] * imarr[3*(x+1+(y+1)*width)]);
+			unsigned int mag = sqrt((pixel_x*pixel_x)+(pixel_y*pixel_y));
+			resultarr[3*(x+y*width)] = mag;
+			resultarr[3*(x+y*width)+1] = mag;
+			resultarr[3*(x+y*width)+2] = mag;
+		}
 
 }
--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 sobel:
-	gcc -D_REENTRANT Lab5.c bitmap.h -o sobel -lpthread -Wall
+	gcc -D_REENTRANT Lab5.c bitmap.h -o sobel -lm -lpthread -Wall
